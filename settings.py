73b4a4079da3fd91029e57c7e9db7055faa91d9a
francoisvercellotti <francoisvercellotti@gmail.com>
2025-01-17 09:02:09 +0100

    adding basic files

---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+# ---------------------- Filepaths ---------------------
+PROJECT_PATH = '/mnt/c/Users/mrgxl/Documents/cours françois/exercice python/Machine Learning/prediction_donnees_immobilieres'
+
+random_state = 42
