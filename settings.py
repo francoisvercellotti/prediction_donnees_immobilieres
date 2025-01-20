a9f4360e8b36a33d897c8f47466911ca2b97ebf9
francoisvercellotti <francoisvercellotti@gmail.com>
2025-01-20 11:05:50 +0100

    adding preprocessing files

--- settings.py
+++ settings.py
@@ -2,4 +2,8 @@
 # ---------------------- Filepaths ---------------------
 PROJECT_PATH = '/mnt/c/Users/mrgxl/Documents/cours françois/exercice python/Machine Learning/prediction_donnees_immobilieres'
 
+# ---------------------- Column names -----------------
+REGRESSION_TARGET = 'prix'
+CLASSIFICATION_TARGET = 'en_dessous_du_marche'
+
 random_state = 42
