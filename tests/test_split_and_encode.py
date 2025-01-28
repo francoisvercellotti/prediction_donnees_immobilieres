92188695b7878e56063923f263a91fd5253bd19d
francoisvercellotti <francoisvercellotti@gmail.com>
2025-01-28 17:04:14 +0100

    supprime les importations inutiles dans test_split_and_encode.py

--- tests/test_split_and_encode.py
+++ tests/test_split_and_encode.py
@@ -1,12 +1,11 @@
 import os
 import sys
+# Ajouter le dossier parent au chemin pour importer les paramètres
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from settings import CLASSIFICATION_TARGET, REGRESSION_TARGET
-
 import pytest
 import pandas as pd
 from src.split_and_encode import split_and_encode
-
 
 
 # Préparer les données pour les tests
